0a532ec0a2290a629f39a41a41bf49f76a9e4982
王俊杰 <danding@wangjunjiedeMacBook-Pro.local>
2016-07-12 20:00:33 +0800

    xx

--- src/structor/components/css/editor/Fake.jsx
+++ src/structor/components/css/editor/Fake.jsx
@@ -3,46 +3,74 @@ import {render} from 'react-dom';
 import '../../../css/fake.css';
 
 
+
+
+
 var Fake=React.createClass({
     keyPressCb:function(event){
         var keycode=event.nativeEvent.keyCode;
-        console.log("value of textarea====\r\n" + event.target.value);
-        console.log("keycode======\r\n" + keycode);
-
+        if(keycode==13)
+            this.state.enter=true;
+        else
+            this.state.enter=false;
     },
     inputCb:function(event){
-        console.log('.....');
-        console.log('......');
-        console.log("value when input =====\r\n" + event.target.value);
+
     },
     keyUpCb:function(event){
         var target=event.target;
-        console.log("selectStart====\r\n" + event.target.selectionStart);
-        console.log("value of textarea======\r\n" + event.target.value);
+        var value=event.target.value;
+        var lreg=/\</g;
+        var rreg=/\>/g;
+        //value = value.replace('\n', '<br/>');
+        //value = value.replace(lreg, '&lt;');
+        //value = value.replace(rreg, '&gt;');
+        var keycode=event.nativeEvent.keyCode;
+        if(keycode==13)//enter key
+        {
+            value+=" ";
+        }
+        else if(keycode==8)//delete key
+        {
+            var lines = event.target.value.split('\n');
+            var line=lines[lines.length-1];
+            var enterReg=/\n$/;
+            if(enterReg.exec(value))
+            {
+                event.target.value=event.target.value.substring(0,event.target.value.length-1);
+            }
+        }
+        this.setState({data: value});
     },
     getInitialState:function()
     {
-        return ({data: ''});
+
+        //<span style={{color:"red"}}>a</span>
+        //<div className="cursor highLight" ref="cursor"></div>
+        return ({data: 'a'});
     },
     render:function(){
-
-     return (
-        <div className="fake">
-            <textarea onKeyUp={this.keyUpCb} onKeyPress={this.keyPressCb} onInput={this.inputCb}></textarea>
-            <div className="back" ref="back" >
-                <span style={{color:"red"}}>a</span>
-                <div className="cursor highLight" ref="cursor"></div>
-            </div>
-        </div>);
+        var data=null;
+        data=this.state.data;
+         return (
+            <div className="fake">
+                <textarea onKeyUp={this.keyUpCb} onKeyPress={this.keyPressCb} onInput={this.inputCb}></textarea>
+                <div className="back" ref="back" >
+                    <pre className="pre">
+                      <div style={{display:"inline-block"}} dangerouslySetInnerHTML={{__html:data}}>
+                      </div>
+                    </pre>
+                </div>
+            </div>);
 
     },
     componentDidMount:function(){
-        var instance=this.refs.cursor;
-        var blink=function() {
-            $(instance).toggleClass("highLight");
-            setTimeout(blink, 500);
-        }
-        setTimeout(blink, 500);
+        //var instance=this.refs.cursor;
+        //var blink=function() {
+        //    $(instance).toggleClass("highLight");
+        //    setTimeout(blink, 500);
+        //}
+        //setTimeout(blink, 500);
 
     }
 });
